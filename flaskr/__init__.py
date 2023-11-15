fab2d1b78a6137b55070a1b1d8ecc99ea6e9a4a7
xBob45 <xmorav45@vutbr.cz>
2023-11-15 21:54:45 +0100

    Initial setup.

---
+++ flaskr/__init__.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_pyfile('config.py', silent=True)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    
+    return app
